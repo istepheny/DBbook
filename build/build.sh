b70feb27ed17a4eee3c59a93d48a62242461e7d9
Stephen Yang <yonshuo@gmail.com>
2020-03-14 20:00:35 +0800

    chore: update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,28 +1,67 @@
 #!/usr/bin/env bash
 
+# Usage:
+# ./build/build.sh 1.0
+
+if [[ -z "${1}" ]]; then
+    echo "version number is required, example: ${0} 1.0"
+    exit 0
+fi
+
+VERSION="v${1}"
+
 SOURCE_DIR=$(cd "$(dirname $(dirname "$0"))"; pwd)
 
-DIST_DIR="${SOURCE_DIR}/dist"
+DIST_DIR="${SOURCE_DIR}/dist/${VERSION}"
 
 mkdir -p ${DIST_DIR}
 
 CONFIG_NAME="database.json"
 
 CONFIG_PATH="${SOURCE_DIR}/${CONFIG_NAME}.example"
-CONFIG_DIST_PATH="${DIST_DIR}/${CONFIG_NAME}"
 
 STATIC_PATH="${SOURCE_DIR}/web/"
-STATIC_DIST_PATH="${DIST_DIR}/web/"
 
-BUILD_NAME="dbbook"
-BUILD_PATH="${DIST_DIR}/${BUILD_NAME}"
+function build() {
+    if [[ "${os}" == "darwin" && "${arch}" == "386" ]] ; then
+        continue
+    fi
 
-if [[ ! -f ${CONFIG_DIST_PATH} ]]; then
+    BUILD_NAME="dbbook"
+
+    if [[ "${os}" == "windows" ]] ; then
+        BUILD_NAME="${BUILD_NAME}.exe"
+    fi
+
+    BUILD_DIR_NAME="dbbook_${os}_${arch}_${VERSION}"
+
+    echo "📦 ${BUILD_DIR_NAME} building..."
+
+    BUILD_DIR_PATH="${DIST_DIR}/${BUILD_DIR_NAME}"
+
+    mkdir -p ${BUILD_DIR_PATH}
+
+    CONFIG_DIST_PATH="${BUILD_DIR_PATH}/${CONFIG_NAME}"
+
     cp ${CONFIG_PATH} ${CONFIG_DIST_PATH}
-fi
 
-rsync -av --delete ${STATIC_PATH} ${STATIC_DIST_PATH}
+    STATIC_DIST_PATH="${BUILD_DIR_PATH}/web/"
 
-cd ${SOURCE_DIR} \
-&& go get -v \
-&& CGO_ENABLED=0 go build -o ${BUILD_PATH}
+    rsync -aq --delete ${STATIC_PATH} ${STATIC_DIST_PATH}
+
+    BUILD_PATH="${BUILD_DIR_PATH}/${BUILD_NAME}"
+
+    cd ${SOURCE_DIR} && CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -o ${BUILD_PATH}
+
+    cd ${DIST_DIR} && zip -r -o -q "${BUILD_DIR_NAME}.zip" ${BUILD_DIR_NAME}
+}
+
+go get -v
+
+for os in windows darwin linux freebsd ; do
+    for arch in 386 amd64; do
+        build
+    done
+done
+
+echo "✅  All done."
